ee70c353d1c8188ed75bce276762c36f2e1ed248
Yuta Ueno <y.ueno0803@gmail.com>
2023-08-08 21:33:26 +0900

    storybook をバージョンアップ
    `npx storybook@latest upgrade` でアップデートした

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,21 @@
+import { dirname, join } from "path";
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
   stories: ["../design/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  addons: [getAbsolutePath("@storybook/addon-links"), getAbsolutePath("@storybook/addon-essentials"), getAbsolutePath("@storybook/addon-interactions")],
   framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
+    name: getAbsolutePath("@storybook/vue3-vite"),
+    options: {}
   },
   docs: {
-    autodocs: "tag",
-  },
+    autodocs: "tag"
+  }
 };
 export default config;
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value) {
+  return dirname(require.resolve(join(value, "package.json")));
+}
