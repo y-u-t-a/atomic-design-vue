f41173273fd86bf45628aa6800170194d79ae605
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2026-08-25 22:23:56 +0900

    storybook を workspace に移動

--- design/.storybook/main.js
+++ design/.storybook/main.js
@@ -2,7 +2,7 @@ import vue from "@vitejs/plugin-vue";
 
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../design/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links"],
   framework: {
     name: "@storybook/vue3-vite",
